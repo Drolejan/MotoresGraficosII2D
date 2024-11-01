ca6f697fc4c9b1deb4db5cb8560ab065a6927340
Alejandro Medina <drolejan@gmail.com>
2024-11-01 13:46:00 -0600

    Update checkpoints.cs

--- Assets/checkpoints.cs
+++ Assets/checkpoints.cs
@@ -6,15 +6,4 @@ public class checkpoints : MonoBehaviour
 {
     public Transform checkpoint;
     public List<Transform> transforms;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
